df7482c736e7a825ee098898527f649dc5a5c4c0
Kostadin Stanev <stanebvns@gmail.com>
2023-09-01 10:11:40 +0300

    moved log count into the state updating func

--- src/components/FunctionalCounter.tsx
+++ src/components/FunctionalCounter.tsx
@@ -10,17 +10,19 @@ const FunctionalCounter: React.FC<IFunctionalCounterProps> = ({
   const [counter, setCounter] = useState(0);
 
   const updateCounter = () => {
-    setCounter((prevState) => prevState + 1);
+    setCounter((prevState) => {
+      console.log('Count about to be updated:', prevState, prevState + 1);
+      return prevState + 1;
+    });
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log('Count about to be updated:', counter, counter + 1);
       updateCounter();
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [counter]);
+  }, []);
 
   const displayElement = useMemo(() => {
     console.log('Count was updated:', counter);
